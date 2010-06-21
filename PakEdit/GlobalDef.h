afc6184286b28f783495d66fdd13a8b2c43edd14
Drajwer <Drajwer@7282d56a-af8b-11de-9f64-4bfc55a448e5>
2010-06-21 16:58:19 +0000

    Code from main tree is now ready to be rewritten to use ORM.

--- PakEdit/GlobalDef.h
+++
@@ -1,9 +0,0 @@
-#ifndef GLOBALDEF_H
-#define GLOBALDEF_H
-
-#define TITLEBAR "openhelbreath Pak Editor"
-#define DIALOG_FILTER "Helbreath Pak (*.pak)\nAll files (*.*)"
-#define DEF_PAKSIG "<Pak file header>"
-#define DEF_SPRSIG "<Sprite File Header>"
-
-#endif // GLOBALDEF_H
