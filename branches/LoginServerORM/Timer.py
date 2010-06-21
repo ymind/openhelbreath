0abb713f60605c9c07275e1af0fa30d576e8a284
Drajwer <Drajwer@7282d56a-af8b-11de-9f64-4bfc55a448e5>
2010-06-21 16:58:19 +0000

    Code from main tree is now ready to be rewritten to use ORM.

---
+++ branches/LoginServerORM/Timer.py
@@ -0,0 +1,76 @@
+""" 
+    This file is part of OpenHelbreath.
+
+    OpenHelbreath is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenHelbreath is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenHelbreath.  If not, see <http://www.gnu.org/licenses/>.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import re, time
+from threading import Thread
+
+class TimerManager(Thread):
+	"""
+		Usage:
+		tm = TimerManager()
+		tm.register_timer(lambda: sys.stdout.write("tactical facepalm"), 'tactical', 0.1, True)
+		
+		register_timer(proc_addr, 'timer_name', interval, autostart)
+		1.0 = 1s
+		0.5 = 0.5s
+		minimum interval = 0.1s
+		etc
+	"""	
+	def __init__(self):
+		Thread.__init__(self)
+		self.timer_list = []
+
+	def register_timer(self, proc, name, tick_time, auto_start = False):
+		self.timer_list += [{'Name': name, 'Time': tick_time, 'Active': auto_start, 'Addr': proc, 'Tick': time.time()}]
+		if len(self.timer_list)>0 and not self.isAlive():
+			self.start()
+
+	def timers(self, proc_name): #Accepting wildcars
+		return filter(lambda x: True if re.match(proc_name, x['Name']) else False, self.timer_list)
+
+	def enable(self, proc_name):
+		for i in self.timers(proc_name):
+			i['Tick'] = time.time()
+			i['Active'] = True
+			
+	def disable(self, proc_name):
+		for i in self.timers(proc_name):
+			i['Active'] = False
+	
+	def run(self):
+		self.running = True
+		while self.running:
+			start = time.time()
+			for i in self.timer_list:
+				if i['Active'] and start - i['Tick']  > i['Time']:
+					ret = i['Addr']()
+					i['Tick'] = start
+					if ret == False:
+						self.timer_list.remove(i)
+			time.sleep(0.1)
---
+++ branches/LoginServerORM/main.py
@@ -0,0 +1,63 @@
+""" 
+    This file is part of OpenHelbreath.
+
+    OpenHelbreath is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenHelbreath is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenHelbreath.  If not, see <http://www.gnu.org/licenses/>.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import sys
+from LoginServer import CLoginServer
+
+def main():
+	print "OpenHelbreath Login Server experimental" # Last stable revision
+	print "Copyright (C) 2009-2010 by openhelbreath team"
+	print "This program comes with ABSOLUTELY NO WARRANTY."
+	print "This is free software, and you are welcome to redistribute it under certain conditions."
+	print
+		
+	Server = CLoginServer()
+	if not Server.DoInitialSetup():
+		print "(!) Stopped!"
+		del Server
+		return False
+	
+	if not Server.InitServer():
+		del Server
+		return False
+		
+	while True:
+		try:
+			q = raw_input(">>> ")
+		except:
+			sys.exit(1)
+		if q != "":
+			Server.CommandHandler(q)
+	
+if __name__ == '__main__':
+	main()
